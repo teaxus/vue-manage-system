5b91c6f86b7226764306fc997aec2fde18a35b46
🐹 <@gmail.com>
2020-09-04 16:39:36 +0800

    action标签的创建

---
+++ src/components/spanner/ActionView.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <div style="color:red">
+        <div>ffffkk_1</div>
+        <div>ffffkk_2</div>
+        <div>ffffkk_3</div>
+    </div>
+    <slot></slot>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'BasicView',
+    mounted() {
+        console.log('basic view');
+    }
+};
+</script>
+
+<style scoped>
+</style>
